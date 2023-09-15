6ad9070fc897edd0aeca25a68ea27bb8415e7e17
jmg292 <jeff@gnzlabs.com>
2023-09-14 23:23:55 -0500

    Error handling for yubikeyring

--- errors/errors.go
+++ errors/errors.go
@@ -1,8 +1,10 @@
 package errors
 
 const (
+	ErrInvalidManagementKey KeyError = "invalid management key"
 	ErrInvalidSigningKey    KeyError = "invalid signing key"
 	ErrInvalidDecryptionKey KeyError = "invalid decryption key"
+	ErrUnsupportedAlgorithm KeyError = "unsupported algorithm"
 )
 
 const (
@@ -11,11 +13,14 @@ const (
 	ErrExportNotAllowed               KeystoreError = "export not allowed"
 	ErrImportNotAllowed               KeystoreError = "import not allowed"
 	ErrKeyAlreadyExists               KeystoreError = "key already exists"
+	ErrKeyNotFound                    KeystoreError = "key not found"
 	ErrFileAlreadyExists              KeystoreError = "file already exists"
 	ErrCertAlreadyExists              KeystoreError = "certificate already exists"
+	ErrCertNotFound                   KeystoreError = "cert not found"
 	ErrUnableToOpenKeystore           KeystoreError = "unable to open keystore"
 	ErrKeystoreLocked                 KeystoreError = "keystore is locked"
 	ErrKeystoreHandleClosed           KeystoreError = "keystore handle is closed"
+	ErrKeystoreNotfound               KeystoreError = "keystore not found"
 	ErrResetNotAllowed                KeystoreError = "reset not allowed"
 	ErrKeyGenFailed                   KeystoreError = "key generation failed"
 )
